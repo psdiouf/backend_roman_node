ead89ae2d0744cc06240172e35d97e61e4d0855f
assane10 <alassdiallo58@gmail.com>
2021-09-06 20:03:48 +0000

    premier commit

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ app.use(bodyParse.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParse.urlencoded({ extends: true }));
-app.use('/', (req, res, next) => {
-    res.end("Bonjour bienvenue");
-})
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/user', users)
 
